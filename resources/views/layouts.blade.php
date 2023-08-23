00f24157bb4242d4278963ab1f6004ce6f63969f
David Eleazar <smartways897@gmail.com>
2023-08-23 22:25:46 +0700

    Fixed scripting
    1. Added 'scripts' stack on main Blade layouts
    2. Fixed scripts typos
    3. Some minor improvements

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -26,9 +26,9 @@
         <li class="nav-item">
           <a class="nav-link" href="{{route('HalamanState')}}">State</a>
         </li>
-    
+
       </ul>
- 
+
     </div>
   </div>
 </nav>
@@ -36,11 +36,14 @@
     <div class="container">
         @yield('konten')
     </div>
+
+    {{-- Load any 3rd party scripts from here --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
-  <script src="//cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"> </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
+    <script src="//cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"> </script>
 
-
+    {{-- Compiled and custom JS are injected here --}}
+    @stack('scripts')
   </body>
 </html>
--- resources/views/pages/country.blade.php
+++ resources/views/pages/country.blade.php
@@ -1,116 +1,105 @@
 @extends('layouts')
 
 @section('konten')
+    <div class="row mt-5">
+    <div class="">
+                <form action="" method="post">
+                    <div class="mb-3">
+                        <label for="formFile" class="form-label">Name</label>
+                        <input class="form-control" name="name" id="name" type="text"  aria-label="default input example">
+                    </div>
+                    <div class="mb-3">
+                        <label for="formFile" class="form-label">Code</label>
+                        <input class="form-control" name="code" id="code" type="text"  aria-label="default input example">
+                    </div>
+                    <div class="mb-3">
+                        <label for="formFile" class="form-label">Continent</label>
+                        <input class="form-control" name="continent" id="continent" type="text"  aria-label="default input example">
+                    </div>
+                    <div class="modal-footer">
 
-<div class="row mt-5">
-<div class="">
-            <form action="" method="post">
-                <div class="mb-3">
-                    <label for="formFile" class="form-label">Name</label>
-                    <input class="form-control" name="name" id="name" type="text"  aria-label="default input example">
-                </div>
-                <div class="mb-3">
-                    <label for="formFile" class="form-label">Code</label>
-                    <input class="form-control" name="code" id="code" type="text"  aria-label="default input example">
-                </div>
-                <div class="mb-3">
-                    <label for="formFile" class="form-label">Continent</label>
-                    <input class="form-control" name="continent" id="continent" type="text"  aria-label="default input example">
-                </div>
-                <div class="modal-footer">
-
-                  <button type="button" class="btn btn-primary tombol-simpan">Save changes</button>
-                </div>
-            </form>
-      </div>
-</div>
-<div class="row ">
-<div class="table-responsive pt-3">
-    <table class="table table-bordered display" id="table_id">
-      <thead>
-        <tr>
-          <th scope="col">No</th>
-          <th scope="col">Name</th>
-          <th scope="col">Code</th>
-          <th scope="col">Continent</th>
-        </tr>
-      </thead>
-   
-    </table>
+                    <button type="button" class="btn btn-primary tombol-simpan">Save changes</button>
+                    </div>
+                </form>
+        </div>
     </div>
-</div>
+    <div class="row ">
+    <div class="table-responsive pt-3">
+        <table class="table table-bordered display" id="table_id">
+        <thead>
+            <tr>
+            <th scope="col">No</th>
+            <th scope="col">Name</th>
+            <th scope="col">Code</th>
+            <th scope="col">Continent</th>
+            </tr>
+        </thead>
 
-<script>
-  $(document).ready(function () {
-    $('#table_id').DataTable({
-      processing: true,
-      serverSide: true,
-      ajax: {
-        "url": 'http://127.0.0.1:8000/api/country',
-      "type": 'GET',
-      "dataType": 'json',
-      "dataSrc": 'data'
-      },
-      columns: [
-        {
-          data:'DT_RowIndex',
-          name:'DT_RowIndex',
-          orederable:false,
-          searchable:false
-        },
-        {
-          data: 'name',
-          name: 'Name'
-        },
-        {
-          data: 'code',
-          name: 'Code'
-        },
-        {
-          data: 'continent',
-          name: 'Continent'
-        },
-     
-      ]
-    });
-  });
-
-  $.ajaxSetup({
-    headers:{
-      'C-CSRF-TOKEN':$('meta[name="csrd-token"]').attr('content')
-    }
-  });
-  $('.tombol-simpan').click(function(){
-         
-         //  console.log(name + '-' + code + '-' + continent);
- 
-         $.ajax(
-           {
-             url:'http://127.0.0.1:8000/api/addcountry',
-             type:'POST',
-             data:{
-               name: $('#name').val(),
-               code: $('#code').val(),
-               continent: $('#continent').val()
-             },
-               success:function(response){
-               
-                 $('#table_id').DataTable().ajax.reload();
-               }
- 
-           }
-         );
- 
-     });
-  $('body').on('click','.tombol-tambah', function(e){
-    e.preventDefault(); 
-    $('#exampleModal').modal('show');
-
-    
-  });
-
-</script>
-
-
-
+        </table>
+        </div>
+    </div>
 @endsection
+
+@push('scripts')
+  <script>
+    $(document).ready(function () {
+      $.ajaxSetup({
+        headers:{
+            'X-CSRF-TOKEN':$('meta[name="csrf-token"]').attr('content')
+        }
+      });
+
+      $('#table_id').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: {
+          "url": 'http://127.0.0.1:8000/api/country',
+          "type": 'GET',
+          "dataType": 'json',
+          "dataSrc": 'data'
+          },
+        columns: [
+          {
+            data:'DT_RowIndex',
+            name:'DT_RowIndex',
+            orederable:false,
+            searchable:false
+          },
+          {
+            data: 'name',
+            name: 'Name'
+          },
+          {
+            data: 'code',
+            name: 'Code'
+          },
+          {
+            data: 'continent',
+            name: 'Continent'
+          }
+        ]
+      });
+
+      $('.tombol-simpan').click(function(){
+        //  console.log(name + '-' + code + '-' + continent);
+        $.ajax({
+          url:'http://127.0.0.1:8000/api/addcountry',
+          type:'POST',
+          data:{
+            name: $('#name').val(),
+            code: $('#code').val(),
+            continent: $('#continent').val()
+          },
+          success:function(response){
+            $('#table_id').DataTable().ajax.reload();
+          }
+        });
+      });
+
+      $('body').on('click','.tombol-tambah', function(e){
+        e.preventDefault();
+        $('#exampleModal').modal('show');
+      });
+    });
+  </script>
+@endpush
